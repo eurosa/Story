af648018535373457b22fb9c4053e5cc9306008d
ranojan <radhasonk.phy@gmail.com>
2019-10-02 11:34:31 +0530

    Initial commit

---
+++ app/src/main/java/com/example/parsaniahardik/json_parse_listview/MainActivity.java
@@ -0,0 +1,198 @@
+package com.example.parsaniahardik.json_parse_listview;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String jsonURL = "https://demonuts.com/Demonuts/JsonTest/Tennis/json_parsing.php";
+    private final int jsoncode = 1;
+    private ListView listView;
+    ArrayList<TennisModel> tennisModelArrayList;
+    private TennisAdapter tennisAdapter;
+
+    private static ProgressDialog mProgressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.lv);
+
+        fetchJSON();
+
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    private void fetchJSON(){
+
+        showSimpleProgressDialog(this, "Loading...","Fetching Json",false);
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.CUPCAKE) {
+            new AsyncTask<Void, Void, String>(){
+                protected String doInBackground(Void[] params) {
+                    String response="";
+                    HashMap<String, String> map=new HashMap<>();
+                    try {
+                        HttpRequest req = new HttpRequest(jsonURL);
+                        response = req.prepare(HttpRequest.Method.POST).withData(map).sendAndReadString();
+                    } catch (Exception e) {
+                        response=e.getMessage();
+                    }
+                    return response;
+                }
+                protected void onPostExecute(String result) {
+                    //do something with response
+                    Log.d("newwwss",result);
+                    onTaskCompleted(result,jsoncode);
+                }
+            }.execute();
+        }
+    }
+
+    public void onTaskCompleted(String response, int serviceCode) {
+        Log.d("responsejson", response.toString());
+        switch (serviceCode) {
+            case jsoncode:
+
+                if (isSuccess(response)) {
+                    removeSimpleProgressDialog();  //will remove progress dialog
+                    tennisModelArrayList = getInfo(response);
+                    tennisAdapter = new TennisAdapter(this,tennisModelArrayList);
+                    listView.setAdapter(tennisAdapter);
+
+
+
+                    listView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+                    {
+                        @Override
+                        public void onItemClick(AdapterView<?> parent, View view,
+                                                int position, long id) {
+                            // TODO Auto-generated method stub
+                            Toast.makeText(MainActivity.this, "Love"+id, Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+                }else {
+                    Toast.makeText(MainActivity.this, getErrorCode(response), Toast.LENGTH_SHORT).show();
+                }
+        }
+    }
+
+    public ArrayList<TennisModel> getInfo(String response) {
+        ArrayList<TennisModel> tennisModelArrayList = new ArrayList<>();
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            if (jsonObject.getString("status").equals("true")) {
+
+                JSONArray dataArray = jsonObject.getJSONArray("data");
+
+                for (int i = 0; i < dataArray.length(); i++) {
+
+                    TennisModel playersModel = new TennisModel();
+                    JSONObject dataobj = dataArray.getJSONObject(i);
+                    playersModel.setName(dataobj.getString("name"));
+                    playersModel.setCountry(dataobj.getString("country"));
+                    playersModel.setCity(dataobj.getString("city"));
+                    playersModel.setImgURL(dataobj.getString("imgURL"));
+                    tennisModelArrayList.add(playersModel);
+
+                }
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return tennisModelArrayList;
+    }
+
+    public boolean isSuccess(String response) {
+
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            if (jsonObject.optString("status").equals("true")) {
+                return true;
+            } else {
+
+                return false;
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public String getErrorCode(String response) {
+
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            return jsonObject.getString("message");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return "No data";
+    }
+
+    public static void removeSimpleProgressDialog() {
+        try {
+            if (mProgressDialog != null) {
+                if (mProgressDialog.isShowing()) {
+                    mProgressDialog.dismiss();
+                    mProgressDialog = null;
+                }
+            }
+        } catch (IllegalArgumentException ie) {
+            ie.printStackTrace();
+
+        } catch (RuntimeException re) {
+            re.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void showSimpleProgressDialog(Context context, String title,
+                                                String msg, boolean isCancelable) {
+        try {
+            if (mProgressDialog == null) {
+                mProgressDialog = ProgressDialog.show(context, title, msg);
+                mProgressDialog.setCancelable(isCancelable);
+            }
+
+            if (!mProgressDialog.isShowing()) {
+                mProgressDialog.show();
+            }
+
+        } catch (IllegalArgumentException ie) {
+            ie.printStackTrace();
+        } catch (RuntimeException re) {
+            re.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/parsaniahardik/json_parse_listview/TennisAdapter.java
@@ -0,0 +1,88 @@
+package com.example.parsaniahardik.json_parse_listview;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class TennisAdapter extends BaseAdapter {
+
+    private Context context;
+    private ArrayList<TennisModel> tennisModelArrayList;
+
+    public TennisAdapter(Context context, ArrayList<TennisModel> tennisModelArrayList) {
+
+        this.context = context;
+        this.tennisModelArrayList = tennisModelArrayList;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return getCount();
+    }
+    @Override
+    public int getItemViewType(int position) {
+
+        return position;
+    }
+
+    @Override
+    public int getCount() {
+        return tennisModelArrayList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return tennisModelArrayList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+
+        if (convertView == null) {
+            holder = new ViewHolder();
+            LayoutInflater inflater = (LayoutInflater) context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.lv_player, null, true);
+
+            holder.iv = convertView.findViewById(R.id.iv);
+            holder.tvname = convertView.findViewById(R.id.name);
+            holder.tvcountry = convertView.findViewById(R.id.country);
+            holder.tvcity = convertView.findViewById(R.id.city);
+
+            convertView.setTag(holder);
+        }else {
+            // the getTag returns the viewHolder object set as a tag to the view
+            holder = (ViewHolder)convertView.getTag();
+        }
+
+        Picasso.get().load(tennisModelArrayList.get(position).getImgURL()).into(holder.iv);
+        holder.tvname.setText("Name: "+tennisModelArrayList.get(position).getName());
+        holder.tvcountry.setText("Country: "+tennisModelArrayList.get(position).getCountry());
+        holder.tvcity.setText("City: "+tennisModelArrayList.get(position).getCity());
+
+        
+
+        return convertView;
+    }
+
+    private class ViewHolder {
+
+        protected TextView tvname, tvcountry, tvcity;
+        protected ImageView iv;
+    }
+
+}
